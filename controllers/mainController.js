2b2289ebcd1d5b84c832ade58f5a0280a08d4dfa
Luis Fernando Arevalo <LuisFerArevalo@Luiss-MacBook-Pro.local>
2024-04-11 16:37:24 -0500

    varias funciones trabajando

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,29 +1,55 @@
 const controller = {}
 const fs = require('fs');
 const data = require('../playersA.json')
+const dataGame = require('../dataGame.json')
 
 
 controller.main = (req,res) => {
   res.render('main')
 }
 
+controller.game = (req,res) => {
+  res.render('game', {
+    data: data
+  })
+}
+
 controller.players = (req,res) => {
   res.render('jugadores', {
-    data: data
+    data: data,
+    dataGame: dataGame
   })
 }
 
 controller.addPlayer = (req,res) => {
 
+  console.log(data)
   data.push(req.body)
-
+  console.log(data)
+  
   fs.writeFile('playersA.json', JSON.stringify(data), (err) => {
     if (err) {
       console.error('Error al guardar el archivo:', err);
       res.status(500).send('Error al guardar el archivo');
     } else {
       console.log('Datos guardados en playersA.json');
-      window.location.reload();
+    }
+
+  });
+      
+}
+
+controller.addGame = (req,res) => {
+
+  dataGame.push(req.body)
+  console.log(dataGame)
+
+  fs.writeFile('dataGame.json', JSON.stringify(dataGame), (err) => {
+    if (err) {
+      console.error('Error al guardar el archivo:', err);
+      res.status(500).send('Error al guardar el archivo');
+    } else {
+      console.log('Datos guardados en dataGame.json');
 
     }
   });
--- routes/main.js
+++ routes/main.js
@@ -4,10 +4,14 @@ const router = express.Router();
 const mainController = require('../controllers/mainController')
 
 router.get('/', mainController.main)
+router.get('/game', mainController.game)
+
 router.get('/jugadores', mainController.players)
 router.get('/cuarto1', mainController.q1)
 
 router.post('/newPlayer', mainController.addPlayer)
+router.post('/newGame', mainController.addGame)
+
 
 
 
